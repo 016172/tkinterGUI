b9e97dbbeeaffabea7565341b266a959f2db4b3a
016172 <seolmi@hotmail.se>
2020-06-13 22:52:02 +0200

    matplotlib implemented

--- start.py
+++ start.py
@@ -1,4 +1,12 @@
+import matplotlib
+matplotlib.use("TkAgg")
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
+from matplotlib.figure import Figure
+
 import tkinter as tk
+from tkinter import ttk
+
 
 LARGE_FONT = ("Verdana", 12)
 
@@ -8,6 +16,10 @@ class BTC(tk.Tk): #Inheritence from tk.Tk
         tk.Tk.__init__(self, *args, **kwargs)
         container = tk.Frame(self)
 
+        #client icon
+        #tk.Tk.iconbitmap(self, default="")
+        tk.Tk.wm_title(self, "BTC Price")
+
         container.pack(side="top", fill="both", expand = True)
 
         container.grid_rowconfigure(0, weight=1)
@@ -15,7 +27,7 @@ class BTC(tk.Tk): #Inheritence from tk.Tk
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, PageOne, PageTwo, PageThree):
             frame = F(container, self)
             self.frames[F] = frame
             frame.grid(row=0, column=0, sticky="nsew") 
@@ -33,37 +45,68 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = tk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
+        button1 = ttk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
         button1.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
         button2.pack()
+
+        button3 = ttk.Button(self, text="Visit Page 3", command=lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 class PageOne(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Visit Page 1", font=LARGE_FONT)
+        label = tk.Label(self, text="Page 1", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = tk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
+        button2 = ttk.Button(self, text="Visit Page 2", command=lambda: controller.show_frame(PageTwo))
         button2.pack()
+
+        button3 = ttk.Button(self, text="Visit Page 3", command=lambda: controller.show_frame(PageThree))
+        button3.pack()
 
 class PageTwo(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Visit Page 2", font=LARGE_FONT)
+        label = tk.Label(self, text="Page 2", font=LARGE_FONT)
         label.pack(pady=10,padx=10)
 
-        button1 = tk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
+        button1 = ttk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
         button1.pack()
 
-        button2 = tk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
+        button2 = ttk.Button(self, text="Visit Page 1", command=lambda: controller.show_frame(PageOne))
         button2.pack()
+
+        button3 = ttk.Button(self, text="Visit Page 3", command=lambda: controller.show_frame(PageThree))
+        button3.pack()
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Graph page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+        button1 = ttk.Button(self, text="Back to home", command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+        f = Figure(figsize=(5,5), dpi=100)
+        a = f.add_subplot(111)
+        a.plot([1,2,3,4,5,6,7,8],[5,6,1,3,8,9,3,5])
+
+        canvas = FigureCanvasTkAgg(f, self)
+        canvas.draw()
+        toolbar = NavigationToolbar2Tk(canvas, self)
+        toolbar.update()
+        canvas.get_tk_widget().pack(side=tk.BOTTOM, fill=tk.BOTH, expand = True)
+        canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand = True)
+
 
 app = BTC()
 app.mainloop()
