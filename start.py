19f5719bcc4908f363d6214ca79e8d0ee64d0170
016172 <seolmi@hotmail.se>
2020-06-13 21:06:29 +0200

    start

--- start.py
+++ start.py
@@ -1,7 +1,39 @@
 import tkinter as tk
 
-class SeaofBTCapp(tk.Tk): #Inheritence from tk.Tk
+LARGE_FONT = ("Verdana", 12)
+
+class BTC(tk.Tk): #Inheritence from tk.Tk
 
     def __init__(self,*args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+
+        container.pack(side="top", fill="both", expand = True)
+
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+
+        frame = StartPage(container, self)
+
+        self.frames[StartPage] = frame
+
+        frame.grid(row=0, column=0, sticky="nsew") #sticky: alignment + stretch, north,south,east,west. Stretch to size of window
+
+        self.show_frame(StartPage)
+    
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+
+app = BTC()
+app.mainloop()
 
